2d87521538a50fe56cf2a1fd3eb62180935d12e0
Yisong Zou <yisongzou@yisong.zou@duke.edu>
2020-08-25 23:13:26 -0400

    Finish hw1 1

--- ECE565_Parallel/hw1/run.sh
+++ ECE565_Parallel/hw1/run.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 #script to run the programs
 
+gcc -O3 -o loop_performance_strip_mining loop_performance_strip_mining.c
 int=1
 while(( $int<=10))
 do
-    ./loop_performance_unrolling 100000000
+    ./loop_performance_strip_mining 100000000
     let "int++"
 done
